c9e0380b1aa47bf0270ad5a56d0dcc1aef0b701d
Stodgers <stodgersm@gmail.com>
2019-07-23 14:07:20 +0800

    ends

--- data/sogou_news/sogou_data_utils.py
+++ data/sogou_news/sogou_data_utils.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import jieba
 #ss = open('news_sohusite_xml.smarty.dat',encoding='utf-8')
-with open('t233.txt','a', encoding='gb18030') as f: # 'a'表示append,即在原来文件内容后继续写数据（不清楚原有数据）
+with open('smarty-t233.txt','a', encoding='gb18030') as f: # 'a'表示append,即在原来文件内容后继续写数据（不清楚原有数据）
     data = open('news_sohusite_xml.smarty.dat', errors='ignore', encoding='gb18030')
     k = 1
     str = ''
@@ -29,11 +29,11 @@ with open('t233.txt','a', encoding='gb18030') as f: # 'a'表示append,即在原�
             if len(str)<=30:
                 k+=1
                 continue
-            f.write(kline + " ".join(jieba.cut(str)))
+            f.write(kline + " ".join(jieba.lcut(str)))
             if flag==0:
                 flag=1
                 kline = '\n\n'
-            print(str)
+            print(" ".join(jieba.lcut(str)))
         else:
             #lst.append(str)
             str = ''
